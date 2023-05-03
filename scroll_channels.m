f8ccd682d0495557a13a2789d799bd4fab4793de
Tommi Väyrynen <105491737+tommivayrynen1@users.noreply.github.com>
2023-05-03 09:35:22 +0300

    Add files via upload

--- scroll_channels.m
+++ scroll_channels.m
@@ -1,10 +1,23 @@
-function f = scroll_channels(raw,lims,varargin)
-    
+% INPUTS:
+% DATA(chan x time)
+% OPTIONAL : ylimits - Default (min max) for each channel separately. 
+% OPTIONAL : second data set to plot of equal size.
+
+function f = scroll_channels(raw,varargin)
+ 
     ognargin = nargin;
-    % chan x time
-    % ylimits
-    % OPTIONAL : second data set to plot of equal size.
-    
+    switch ognargin
+        case 1
+            lims(:,1) = min(raw') % separate limits for each channel.
+            lims(:,2) = max(raw')
+        case 2
+            lims(:,1) = repmat(varargin{1}(1),[size(raw,1),1]);
+            lims(:,2) = repmat(varargin{1}(2),[size(raw,1),1]);
+        case 3
+            lims(:,1) = repmat(varargin{1}(1),[size(raw,1),1]);
+            lims(:,2) = repmat(varargin{1}(2),[size(raw,1),1]);
+    end
+
     f = figure('Position',[ 119 1063 2277 509]);
     
     ax=axes(f)
@@ -24,14 +37,14 @@ function f = scroll_channels(raw,lims,varargin)
     
     
     
-    plot(ax,raw(1,:),'Color','k');ylim(ax,lims);xlim(ax,[1 size(raw,2)])
+    plot(ax,raw(1,:),'Color','k');ylim(ax,lims(1,:));xlim(ax,[1 size(raw,2)])
     p2 = plot(zoomed,raw(1,1:round(size(raw,2)/50)),'Color','k');;ylim(zoomed,get(zoomed,'YLim'));xlim(zoomed,[1 round(size(raw,2)/50)])
 
     
     a=annotation('textbox',[.92 .65 .3 .3],'String',sprintf('Channel: %i / %i',1,size(raw,1)),'FitBoxToText','on');
 
     if ognargin > 2
-        hold on;plot(varargin{1}(1,:),'Color',[.6 .6 .6]);end
+        hold on;plot(varargin{2}(1,:),'Color',[.6 .6 .6]);end
     
         function SliderChangeFunction(hObject,eventdata)
           delete(a)
@@ -39,11 +52,11 @@ function f = scroll_channels(raw,lims,varargin)
           [~,ind2plot]=min(abs((hObject.Value - slidervalues)));      
           a=annotation('textbox',[.92 .65 .3 .3],'String',sprintf('Channel: %i / %i',ind2plot,size(raw,1)),'FitBoxToText','on');
           cla
-          plot(ax,raw(ind2plot,:),'Color','k');ylim(ax,lims);xlim(ax,[1 size(raw,2)])
+          plot(ax,raw(ind2plot,:),'Color','k');ylim(ax,lims(ind2plot,:));xlim(ax,[1 size(raw,2)])
           p2 = plot(zoomed,raw(ind2plot,1:round(size(raw,2)/50)),'Color','k');ylim(zoomed,get(zoomed,'YLim'));xlim(zoomed,[1 round(size(raw,2)/50)])
 
           if ognargin > 2
-            hold on;plot(varargin{1}(ind2plot,:),'Color',[.6 .6 .6]);end
+            hold on;plot(varargin{2}(ind2plot,:),'Color',[.6 .6 .6]);end
 
         end
     
